3ee099669dec38ead5e9d7cf596dfabc4ead410a
AnaLinsDev <analinsdev@gmail.com>
2021-07-31 17:24:17 -0300

    preparando o ambiente

---
+++ src/test/java/br/edu/ifpb/gps/planner/Planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.gps.planner.Planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
